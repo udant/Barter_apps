6c0f691e290d22ae7c1cb2729e7fb577f72b161c
udant <61499772+udant@users.noreply.github.com>
2020-08-09 22:23:11 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import SignupLoginScreen from './screens/signupLoginScreen';
 export default function App() {
   return (
     <View style={styles.container}>
-
+      <SignupLoginScreen/>
     </View>
   );
 }
