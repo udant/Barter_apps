5290cf8210669cacfb33fdf712a230978a2c4b1c
udant <61499772+udant@users.noreply.github.com>
2020-08-16 18:23:46 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { YellowBox } from "react-native";
+import _ from "lodash";
+YellowBox.ignoreWarnings(["Setting a timer"]);
+const _console = _.clone(console);
+console.warn = (message) => {
+if (message.indexOf("Setting a timer") <= -1) {
+_console.warn(message);
+}
+};
+
 import SignupLoginScreen from './screens/signupLoginScreen';
 export default function App() {
   return (
